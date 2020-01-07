7b5631c666d9d257ddc3ede6233d5b5ce7e91e41
prattya datta <pratt@LAPTOP-500LE5P5.(none)>
2020-01-07 16:54:25 +0100

    test commit with packages

---
+++ src/components/backend/views/header.php
@@ -0,0 +1,33 @@
+<?php?>
+<!-- Navigation Block -->
+<div class="bloc l-bloc" id="nav-bloc">
+	<div class="container">
+		<nav class="navbar row">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">Company</a>
+				<button id="nav-toggle" type="button" class="ui-navbar-toggle navbar-toggle" data-toggle="collapse" data-target=".navbar-1">
+					<span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+				</button>
+			</div>
+			<div class="collapse navbar-collapse navbar-1">
+				<ul class="site-navigation nav">
+					<li>
+						<a href="#">about</a>
+					</li>
+					<li>
+						<a href="#">team</a>
+					</li>
+					<li>
+						<a href="#">projects</a>
+					</li>
+					<li>
+						<a href="#">feed</a>
+					</li>
+					<li>
+						<a href="#">contacts</a>
+					</li>
+          </ul>
+			</div>
+		</nav>
+	</div>
+</div>
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const webpack = require('webpack');
+const { join, resolve } = require('path');
+const HTMLWebpackPlugin  = require('html-webpack-plugin');
+var path = require('path');
+//const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+//require('infinite-scroll');
+require('es6-promise').polyfill();
+//const{ axiosPlugin } = require('vue-axios-plugin');
+module.exports= {
+    entry: './src/index.js',
+    output:{
+        path: __dirname + '/dist',
+        filename:'bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: {
+                            minimize: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader'
+                }
+            },
+            {
+                test:/\.(scss)$/,
+                use:[
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        //interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader:'css-loader'
+                    },
+                    {
+                        //loader for webpack to process css with postcss
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function(){
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    {
+                        loader:'sass-loader'
+                    }
+                    
+                ]
+            },
+            { test: /\.(jpe?g|png|gif)$/i, loader:"file" },
+        ]
+    },
+    resolve: {
+        alias:{
+            // bind version of jquery-ui
+            "jquery-ui": "jquery-ui/jquery-ui.js",      
+            // bind to modules;
+            modules: path.join(__dirname, "node_modules"),
+        }
+    },
+    plugins:[
+        new HTMLWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery":"jquery"
+          })
+    ]
+}; 
