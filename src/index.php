c8b2c925e99fad730245cc21e4bf817413d4267c
prattya datta <pratt@LAPTOP-500LE5P5.(none)>
2020-01-08 09:53:48 +0100

    creating databases

--- src/index.php
+++ src/index.php
@@ -1,1 +1,12 @@
-<?php?>
+<!Doctype html>
+<html lang=en>
+    <head>
+    <title>A Finder Website</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" crossorigin="anonymous">
+    </head>
+    <body>
+        <?php include('./components/backend/views/header.php') ?>
+    </body>
+</html>
